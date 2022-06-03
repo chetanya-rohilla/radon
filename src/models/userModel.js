be3ba815fda7e277016a4c2dd8859b614024aeb0
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-04 00:57:09 +0530

    pritesh sir

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,26 +1,30 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
+const userSchema = new mongoose.Schema( 
+  {
+    firstName: {
+      type : String,
+      required : true
+    },
     lastName: String,
     mobile: {
-        type: String,
+        type: String, //+91 (2041) 854
         unique: true,
         required: true
     },
     emailId: String,
     gender: {
         type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+        enum: ["male", "female", "LGBTQ"] 
     },
     age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    isIndian: Boolean,
+    parentsInfo: {
+        motherName: String,
+        fatherName: String,
+        siblingName: String
+    },
+    cars: [ String  ]
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema) //users
