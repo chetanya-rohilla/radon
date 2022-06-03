3bff12afc94769968f25f2d8a7261ae964eaf328
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-03 12:47:17 +0530

    post api

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My third api!')
+router.post('/test-post', function (req, res) {
+    let arr = [12, "functionup"]
+    let ele = req.body.element
+    arr.push(ele)
+
+    console.log(req.body);
+    res.send( { msg : arr } )
 });
 
 module.exports = router;
