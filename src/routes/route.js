7bd8369fec2f653ac2172313ac7333215abf796a
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-03 11:30:08 +0530

    lab session

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    res.send('My third api!')
 });
 
 module.exports = router;
