434b09123777bd9f86a1be99bba0ab3e66e1db98
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-08 15:05:32 +0530

    optional problems

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -44,8 +44,40 @@ const priceBooks = async function(req,res){
 
 
 
+// Optional Problems
+
+const getBookAuthor = async function(req,res){
+    let data = req.body.authorName
+    let foundAuthor = await AuthorModel.findOne({"author_id" : req.params.id})
+    let books = await BookModel.find({ "author_id" : foundAuthor.author_id})
+
+    res.send({ msg : books })
+}
+
+const authorAge = async function(req,res){
+    let foundAuthor = await AuthorModel.find({"age" : { $gt : 50 }})
+    let author = []
+    let age = []
+
+    for(let i=0; i< foundAuthor.length ; i++){
+        let books = await BookModel.find({ "author_id" : foundAuthor[i].author_id})
+        books.forEach(ele => {
+            if(ele.rating > 4 && !author.find(ele => ele == foundAuthor[i].authorName )){
+                author.push(foundAuthor[i].authorName)
+                age.push(foundAuthor[i].age)
+            }
+        })
+    }
+
+    res.send({ msg : [author, age] })
+}
+
+
+
 module.exports.createBook = createBook
 module.exports.createAuthor = createAuthor
 module.exports.getBook = getBook
 module.exports.updatePrice = updatePrice
 module.exports.priceBooks = priceBooks
+module.exports.getBookAuthor = getBookAuthor
+module.exports.authorAge = authorAge
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,5 +7,7 @@ router.post('/createBook', BookController.createBook)
 router.post('/getBook', BookController.getBook)
 router.post('/updatePrice', BookController.updatePrice)
 router.get('/priceBooks', BookController.priceBooks)
+router.get('/books-by-authorid/:id', BookController.getBookAuthor)
+router.get('/authorAge', BookController.authorAge)
 
 module.exports = router
