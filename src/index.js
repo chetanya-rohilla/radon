7ed5b1b1ae36905dfa395b2e0156e1e444bacb2d
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-13 20:14:55 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
-const { default: mongoose } = require('mongoose');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const route = require('./routes/route.js')
+const app = express()
+const { default: mongoose } = require('mongoose')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
+app.use('/', route)
 
+mongoose.connect("mongodb+srv://chetanya-rohilla:chetanya@cluster0.jsshkkh.mongodb.net/Chetanya-DB")
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.log(err))
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
-    useNewUrlParser: true
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express app running in port "+ (process.env.PORT || 3000));
 })
-.then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
-
-
-
-app.use('/', route);
-
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        res.send({msg:"done"})
-  }
-  );
-
-
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
-});
