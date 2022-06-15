9732a8ad91ff5cce19ba6a0b333d9b82430becb3
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-15 22:44:27 +0530

    optimized

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,10 +1,14 @@
 const jwt = require("jsonwebtoken");
 
-const checkToken = async function(req,res,next){
+const checkToken = function(req,res,next){
     let token = req.headers["x-auth-token"]
     if(!token)  return res.send({ status: false, msg: "Please pass a token" })
-    const decodedToken = await jwt.verify(token, "functionup-radon")
+
+    const decodedToken = jwt.verify(token, "functionup-radon")
     if(!decodedToken) return res.send({ status: false, msg: "Invalid token" })
+
+    if(decodedToken.userId !== req.params.userId)    return res.send("Trying to access data of different user")
+
     next()
 }
 module.exports.checkToken = checkToken
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,14 +2,13 @@ const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
 const tokenMid= require("../middlewares/auth")
-const userMid= require("../middlewares/auth2")
 
 router.post("/users", userController.createUser  )
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", tokenMid.checkToken, userMid.userAuth, userController.getUserData)
-router.put("/users/:userId", tokenMid.checkToken, userMid.userAuth, userController.updateUser)
-router.delete("/users/:userId", tokenMid.checkToken, userMid.userAuth, userController.deleteUser)
+router.get("/users/:userId", tokenMid.checkToken, userController.getUserData)
+router.put("/users/:userId", tokenMid.checkToken, userController.updateUser)
+router.delete("/users/:userId", tokenMid.checkToken, userController.deleteUser)
 
 module.exports = router;
