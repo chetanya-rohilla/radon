1fcd6db2a90bf15539ed8a07f1ff9b38627e1efc
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-06-17 18:12:42 +0530

    try catch

--- src/controllers/memeController.js
+++ src/controllers/memeController.js
@@ -1,24 +1,34 @@
 let axios = require("axios")
 
 const getAllMemes = async function(req,res){
-    const options = {
-        "method" : "get",
-        "url" : "https://api.imgflip.com/get_memes"
+    try {
+        const options = {
+            "method" : "get",
+            "url" : "https://api.imgflip.com/get_memes"
+        }
+    
+        const result = await axios(options)
+        res.status(200).send({msg : result.data})
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
     }
 
-    const result = await axios(options)
-    res.status(200).send({msg : result.data})
 }
 
 const captionImage = async function(req,res){
-    const {template_id, text0, text1, username, password} = req.query
-    const options = {
-        "method" : "get",
-        "url" : `https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`
+    try {
+        const {template_id, text0, text1, username, password} = req.query
+        const options = {
+            "method" : "get",
+            "url" : `https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`
+        }
+    
+        const result = await axios(options)
+        res.status(200).send({msg : result.data})
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
     }
 
-    const result = await axios(options)
-    res.status(200).send({msg : result.data})
 }
 
 module.exports.getAllMemes = getAllMemes
--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -1,44 +1,59 @@
 let axios = require("axios")
 
 let getWeather = async function(req,res){
-    let city = req.query.city
-    const options = {
-        "method" : "get",
-        "url" : `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3fa3696c3595e0638660f452c9660ee7`
-    }
+    try {
+        let city = req.query.city
+        const options = {
+            "method" : "get",
+            "url" : `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3fa3696c3595e0638660f452c9660ee7`
+        }
 
-    const result = await axios(options)
-    res.status(200).send({msg : result.data})
+        const result = await axios(options)
+        res.status(200).send({msg : result.data})
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
+    
 }
 
 let onlyTemp = async function(req,res){
-    let city = req.query.city
-    const options = {
-        "method" : "get",
-        "url" : `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3fa3696c3595e0638660f452c9660ee7`
-    }
+    try {
+        let city = req.query.city
+        const options = {
+            "method" : "get",
+            "url" : `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3fa3696c3595e0638660f452c9660ee7`
+        }
 
-    const result = await axios(options)
-    res.status(200).send({msg : result.data.main.temp})
+        const result = await axios(options)
+        res.status(200).send({msg : result.data.main.temp})
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
+    }
+    
 }
 
 const sortByTemp = async function(req,res){
-    let cities = ["Bengaluru", "Mumbai", "Delhi", " Kolkata", "Chennai", "London", "Moscow"]
-    let cityData = []
+    try {
+            let cities = ["Bengaluru", "Mumbai", "Delhi", " Kolkata", "Chennai", "London", "Moscow"]
+        let cityData = []
 
-    for(let i=0; i < cities.length; i++){
-        const options = {
-            "method" : "get",
-            "url" : `http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=3fa3696c3595e0638660f452c9660ee7`
+        for(let i=0; i < cities.length; i++){
+            const options = {
+                "method" : "get",
+                "url" : `http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=3fa3696c3595e0638660f452c9660ee7`
+            }
+
+            const result = await axios(options);
+            cityData.push({city:cities[i], temp:result.data.main.temp})
         }
-
-        const result = await axios(options);
-        cityData.push({city:cities[i], temp:result.data.main.temp})
+        let sortedCity = cityData.sort((a,b) => {
+            return a.temp - b.temp
+        })
+        res.status(200).send({msg : sortedCity})
+    } catch (error) {
+        res.status(500).send({ msg: error.message })
     }
-    let sortedCity = cityData.sort((a,b) => {
-        return a.temp - b.temp
-    })
-    res.status(200).send({msg : sortedCity})
+    
 }
 
 module.exports.getWeather = getWeather
